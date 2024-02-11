14ef6e959508319e5ef764f05e4b643d8e57c17f
Eofre <yegorgordinskiy2002@gmail.com>
2024-02-12 01:37:29 +0800

    fix

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,13 +1,13 @@
 import { ReduxProvider } from "./ReduxProvider";
 import { Router } from "./RouterProvider";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 export function Provider() {
   return (
     <ReduxProvider>
-      <HashRouter>
+      <BrowserRouter>
         <Router />
-      </HashRouter>
+      </BrowserRouter>
     </ReduxProvider>
   );
 }
