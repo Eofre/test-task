3558fc0939ebca6d5f46143e7463d3f96018a517
Eofre <yegorgordinskiy2002@gmail.com>
2024-02-12 02:25:10 +0800

    fix

--- src/shared/ui/UiButtonGoBack.tsx
+++ src/shared/ui/UiButtonGoBack.tsx
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router-dom";
 import { UiButton } from ".";
 import backIcon from "assets/back.svg";
 import { useResize } from "shared/lib";
+import { ROUTES } from "shared/constants";
 
 export function UiButtonGoBack() {
   const { width } = useResize();
   const navigate = useNavigate();
   const handleGoBack = () => {
-    navigate(-1);
+    // navigate(-1);
+    navigate(ROUTES.HOME);
   };
   const isMobile = width < 768;
   return (
